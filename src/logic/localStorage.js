12b6820e22115a30ba1f94b56fc0144ce08f08fa
Zeeshan Haider <zhadier39@gmail.com>
2022-03-08 01:07:46 +0500

    add local storage since limited calls to api can be made

--- src/logic/localStorage.js
+++ src/logic/localStorage.js
@@ -2,7 +2,7 @@ export const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
     if (serializedState === null) {
-      return [];
+      return null;
     }
     return JSON.parse(serializedState);
   } catch (err) {
