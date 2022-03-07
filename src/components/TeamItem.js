227825c5f97b3c535401571634ada26456a3ef1b
Zeeshan Haider <zhadier39@gmail.com>
2022-03-08 01:06:49 +0500

    Make team item component to display team in homepage

---
+++ src/components/TeamItem.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+
+const TeamItem = (props) => {
+  const {
+    id, name, logo, rank,
+  } = props;
+
+  return (
+    <li className="rocket-container" id={id}>
+      <Link to="/teamdetails/?id={id}">
+        <img src={logo} alt="team-logo" />
+        <div className="team-container">
+          <h2>{name}</h2>
+          <p>
+            Rank :
+            {' '}
+            {rank}
+          </p>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+TeamItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  logo: PropTypes.string.isRequired,
+  rank: PropTypes.number.isRequired,
+};
+
+export default TeamItem;
