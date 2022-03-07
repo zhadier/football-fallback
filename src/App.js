16c9463ab636e72c409a2e2dbe13b80966495455
Zeeshan Haider <zhadier39@gmail.com>
2022-03-08 03:10:08 +0500

    update routes to be able to access all the different teams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
+import TeamDetails from './components/TeamDetails';
 import Homepage from './components/Homepage';
 
 const App = () => (
@@ -8,7 +9,7 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* <Route path="/teamdetails/:id" element={<Teamdetails />} /> */}
+        <Route path="/teamdetails/:id" element={<TeamDetails />} />
       </Routes>
     </BrowserRouter>
   </Provider>
